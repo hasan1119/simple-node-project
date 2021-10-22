47b36b3c7104142d6b557899b01f3f04ca5c2e0b
Md. Rokibul Hasan <hasan63251119@gmail.com>
2021-10-22 17:38:48 +0600

    api creation

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 // import package
-const express = require('express')
-
+const express = require("express");
 
 // initialize package
-const app = express()
+const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('hello world!')
-})
+app.get("/", (req, res) => {
+  res.send("Oh ho, hello world!");
+});
+
+const users = [
+  {
+    id: 1,
+    name: "Md. Rokibul Hasan",
+    email: "Hasan.webappsdeveloper@gmail.com",
+    phone: "01763251119",
+  },
+  {
+    id: 2,
+    name: "Mehedi Hasan",
+    email: "Mehedi.webappsdeveloper@gmail.com",
+    phone: "01763251119",
+  },
+  {
+    id: 3,
+    name: "Zubair Ahmed",
+    email: "Zubair.webappsdeveloper@gmail.com",
+    phone: "01763251119",
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+
+
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    console.log(userId);
+    res.send(users[userId])
+});
 
 
 
 // listening method
 app.listen(port, () => {
-    console.log('listening from port', 3000);
-})
+  console.log("listening from port", 3000);
+});
